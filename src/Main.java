9a84885fa0c97ef1b221dccc3350dc4906d12310
Alin Piero Javier Cruz <alin.javier@entelgy.pe.local>
2023-03-30 10:18:47 -0500

    Clase y metodo

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        SumaNumeros SN = new SumaNumeros();
+
+        System.out.println("La suma es: "+SN.suma);
+    }
+}
---
+++ src/SumaNumeros.java
@@ -0,0 +1,5 @@
+public class SumaNumeros {
+    int a=5;
+    int b=4;
+    int suma=a+b;
+}
